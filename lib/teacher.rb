f38d36aa0d6b6135ee647930fc7d045cd21e2c91
Chris Nady <nady.christopher@gmail.com>
2017-09-12 12:25:49 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -2,8 +2,10 @@ class Teacher<User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  prng = Random.new
+
   def teach
-    KNOWLEDGE[math.random(0,6)]
+    KNOWLEDGE[prng(0..7)]
   end
 
 end
